64b4d87008aade13bfe50cbfc2de401fffc1e248
Edward <edwardgonsalves24@gmail.com>
2018-07-22 05:31:54 +0530

    media

--- nodes.py
+++ nodes.py
@@ -6,15 +6,7 @@ class Program:
         self.previous = 0
         self.current_transactions = []
 
-    def new_transaction(self, sender, recipient, amount):
-        """
-        Creates a new transaction to go into the next mined Block
-
-        :param sender: Address of the Sender
-        :param recipient: Address of the Recipient
-        :param amount: Amount
-        :return: The index of the Block that will hold this transaction
-        """
+    def new_transaction(self, amount):
         self.current_transactions.append({
             'id': self.id,
             'amount': amount,
@@ -28,3 +20,10 @@ class Media:
         self.init = 0
         self.previous = 0
         self.current_transactions = []
+
+    def new_transaction(self, amount):
+        self.current_transactions.append({
+            'id': self.id,
+            'amount': amount,
+        })
+        # Call Chain
