22ed2c9a0bc1d2d8a8cb9cb8f394c1562b31762a
Edward <edwardgonsalves24@gmail.com>
2018-07-22 05:38:31 +0530

    new_node

--- nodes.py
+++ nodes.py
@@ -1,3 +1,9 @@
+avg_finance_percent = 0.6
+avg_childcare_percent = 0.2
+init_media1 = 50000
+init_media2 = 50000
+
+
 class Program:
     def __init__(self):
         self.id = ""
@@ -13,6 +19,11 @@ class Program:
         })
         # Call Chain
 
+    def new_node(self, share, name):
+        self.share = share
+        self.id = name
+        # Call Chain
+
 
 class Media:
     def __init__(self):
@@ -26,4 +37,9 @@ class Media:
             'id': self.id,
             'amount': amount,
         })
+        # Call Chain
+
+    def new_node(self, init, name):
+        self.init = init
+        self.id = name
         # Call Chain
